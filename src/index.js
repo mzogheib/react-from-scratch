5b0013c731bd90c663e01c12b2e91704d140a2fc
Marwan Zogheib <mzogheib@gmail.com>
2019-01-13 18:01:45 +1100

    Format code according to prettier and eslint config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './components/App'
-import { Helmet } from 'react-helmet';
-import favicon from './favicon.png';
+import { Helmet } from 'react-helmet'
+import favicon from './favicon.png'
 
-const Head = () =>
+const Head = () => (
   <Helmet>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -12,11 +12,13 @@ const Head = () =>
     <link rel="apple-touch-icon" href={favicon} />
     <title>React from Scratch</title>
   </Helmet>
+)
 
-const Root = () =>
+const Root = () => (
   <div>
     <Head />
     <App />
   </div>
+)
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'))
